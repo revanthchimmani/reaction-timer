ba18167a037d8ebd9bbe4f5f24c0b75bde634750
Revanth Chimmani <revanthchimmani@gmail.com>
2022-03-09 16:50:58 -0700

    Added Block Vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
   <h1>Reaction Timer Game</h1>
+  <button @click="startTimer" :disabled="isPlaying">Play</button>
+  <p v-if="userReactionScore && !isPlaying">
+    Reaction time : {{ userReactionScore }}ms
+  </p>
+  <Block v-if="isPlaying" :delay="delay" @userTime="userReactionTime" />
 </template>
 
 <script>
+import Block from "./components/Block.vue";
 
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      userReactionScore: null,
+    };
+  },
+  methods: {
+    startTimer() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+    },
+    userReactionTime(reactionTime) {
+      this.userReactionScore = reactionTime;
+      this.isPlaying = false;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Block.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">Catch Me</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: 0,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log(this.reactionTime);
+      this.$emit("userTime", this.reactionTime);
+    },
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
